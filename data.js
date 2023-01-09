cfb4a6e07b5b57d8a57b66c137f4f8b9cecace73
Mubeen Naeem <122147749+mubeenn1@users.noreply.github.com>
2023-01-09 23:18:47 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,33 @@
+const main = {
+  name: 'Mubeen Naeem',
+  mail: 'mubeennaeem234@gmail.com',
+  img: 'https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg',
+  role: ['Database Developer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Home',
+      iconName: 'logo-firefox',
+      link: 'https://mubeenn1.github.io'
+    },
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/mubeenn1'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/mubeen-naeem-3431b7140/'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'mubeennaeem234@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: '#projects' },
+    { name: 'Blogs', link: '#blogs' }
+  ]
+};
